ff420ef197f139c0eacd4266b49ba5c80b37feda
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2023-12-19 18:34:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@
                        else if(test.value == "EH0104602"){
                     window.location.href="https://drive.google.com/uc?id=1NrJqrLKJxQoli80QKaBrQBMZluEdEM0r"
                }
+                                else if(test.value == "10703060"){
+                    window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
+               }
             else{
                  alert("invalid Passport")
             }
