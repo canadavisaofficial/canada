b1f04751908bad6613a7b17751c65e7d32aa0452
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2023-12-20 19:58:41 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-        
+   function redirect(){
+            var test =  document.querySelector('#passport');
+       if(test.value == "demo123"){
+                    window.location.href="demo.html" 
         function redirect(){
             var test =  document.querySelector('#passport');
        if(test.value == "demo123"){
-                    window.location.href="demo.html"
+                    window.location.href="sofulla.html"
                }  
                else if(test.value == "123"){
                     window.location.href="test.html"
