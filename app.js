35a63b31c7490168c668dbd454c2fe36799caff9
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2023-12-21 20:22:25 +0600

    app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
- function redirect(){
-            var test =  document.querySelector('#passport');
-       if(test.value == "demo123"){
-                    window.location.href="demo.html"
-               }  
-               else if(test.value == "123"){
-                    window.location.href="test.html"
-               }  
-                       else if(test.value == "A00988943"){
-                    window.location.href="saiful.html"
-               }
-                    else if(test.value == ""){
-                    window.location.href=""
-               }
+function redirect(){
+    var test =  document.querySelector('#passport');
+if(test.value == "demo123"){
+            window.location.href="demo.html"
+       }  
+       else if(test.value == "123"){
+            window.location.href="test.html"
+       }  
+               else if(test.value == "A00988943"){
+            window.location.href="saiful.html"
+       }
+            else if(test.value == ""){
+            window.location.href=""
+       }
 
-                            else if(test.value == "A02417406"){
-                    window.location.href="https://drive.google.com/uc?id=1WE6Cyn7FhexfmiuXF6uggkF7174EWcsw
-"
-               }
-                       else if(test.value == "10703060"){
-                    window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
-               }
-                                         else if(test.value == "107030603"){
-                    window.location.href="jakir.html"
-               }
-                                                     else if(test.value == "779966"){
-                    window.location.href="mostafiz.html"
-               }
-            else{
-                 alert("invalid Passport")
-            }
+                    else if(test.value == "A02417406"){
+            window.location.href="https://drive.google.com/uc?id=1WE6Cyn7FhexfmiuXF6uggkF7174EWcsw"
+       }
+               else if(test.value == "10703060"){
+            window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
+       }
+                                 else if(test.value == "107030603"){
+            window.location.href="jakir.html"
+       }
+                                             else if(test.value == "779966"){
+            window.location.href="mostafiz.html"
+       }
+    else{
+         alert("invalid Passport")
+    }
+
+      }
+       
+     
    
-              }
-               
-             
-           
-         
-          
+ 
+  
