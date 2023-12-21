75e21018c51b582462f1ba8ff2a93c1cfa985a5b
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2023-12-21 20:33:22 +0600

    app.js

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-function redirect(){
-    var test =  document.querySelector('#passport');
-if(test.value == "demo123"){
-            window.location.href="demo.html"
-       }  
-       else if(test.value == "123"){
-            window.location.href="test.html"
-       }  
-               else if(test.value == "A00988943"){
-            window.location.href="saiful.html"
-       }
-       //      else if(test.value == ""){
-       //      window.location.href=""
-       // }
-
-                    else if(test.value == "A02417406"){
-            window.location.href="https://drive.google.com/uc?id=1WE6Cyn7FhexfmiuXF6uggkF7174EWcsw"
-       }
-               else if(test.value == "10703060"){
-            window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
-       }
-                                 else if(test.value == "107030603"){
-            window.location.href="jakir.html"
-       }
-                                             else if(test.value == "779966"){
-            window.location.href="mostafiz.html"
-       }
-    else{
-         alert("invalid Passport")
-    }
-
-      }
-       
-     
+ function redirect(){
+            var test =  document.querySelector('#passport');
+       if(test.value == "demo123"){
+                    window.location.href="demo.html"
+               }  
+               else if(test.value == "123"){
+                    window.location.href="test.html"
+               }                                      
+            else{
+                 alert("invalid Passport")
+            }
    
- 
-  
+              }
