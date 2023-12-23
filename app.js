c4b4b759586271fca5a85d49e4da5620ed5188e2
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2023-12-23 15:33:25 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
- function redirect(){
-            var test =  document.querySelector('#passport');
-       if(test.value == "demo123"){
-                    window.location.href="demo.html"
-               }  
-               else if(test.value == "123"){
-                    window.location.href="test.html"
-               }  
-                else if(test.value == "01839493"){
-                    window.location.href="naim.html"
-               }  
-                 else if(test.value == "A"){
-            window.location.href="sa.html"
-       }
-                   else if(test.value == "A00988943"){
-            window.location.href="mdsaifulislamcanadavisa.html"
-       }
-           else if(test.value == "A00988943"){
-            window.location.href="saifulislamcandavisaaa.html"
-       }
-      
-                   else if(test.value == "A02417406"){
-            window.location.href="sofullacanada.html"
-       }
-                 else if(test.value == "A02805864"){
-            window.location.href="mdayatullah.html"
-       }
+function redirect(){
+     var test =  document.querySelector('#passport');
+if(test.value == "demo123"){
+             window.location.href="demo.html"
+        }  
+        else if(test.value == "123"){
+             window.location.href="test.html"
+        }  
+         else if(test.value == "01839493"){
+             window.location.href="naim.html"
+        }  
+          else if(test.value == "A"){
+     window.location.href="sa.html"
+}
+            else if(test.value == "A00988943"){
+     window.location.href="mdsaifulislamcanadavisa.html"
+}
+    else if(test.value == "A00988943"){
+     window.location.href="saifulislamcandavisaaa.html"
+}
 
-                   else if(test.value == "A00988943"){
-            window.location.href="mohammadsaifulislam.html"
+            else if(test.value == "A02417406"){
+     window.location.href="sofullacanada.html"
+}
+          else if(test.value == "A02805864"){
+     window.location.href="mdayatullah.html"
+}
+
+            else if(test.value == "A00988943"){
+     window.location.href="mohammadsaifulislam.html"
+}
+             else if(test.value == "10703060"){
+     window.location.href="julkar.html"
+}
+     else if(test.value == "AOO988943"){
+     window.location.href="saifulislamcanada.html"
+}  
+              else if(test.value == "A02417406"){
+     window.location.href="https://drive.google.com/uc?id=1WE6Cyn7FhexfmiuXF6uggkF7174EWcsw"
+}
+          else if(test.value == "A00988943"){
+     window.location.href="https://drive.google.com/uc?id=1o5tET7-EjUgGqv_HCTOKI-jBk621UvGW"
+}
+              else if(test.value == "10703060"){
+     window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
+}
+               else if(test.value == "107030603"){
+     window.location.href="jakir.html"
+}
+                else if(test.value == "779966"){
+     window.location.href="mostafiz.html"
+}
+     else{
+          alert("invalid Passport")
+     }
+
        }
-                    else if(test.value == "10703060"){
-            window.location.href="julkar.html"
-       }
-            else if(test.value == "AOO988943"){
-            window.location.href="saifulislamcanada.html"
-       }  
-                     else if(test.value == "A02417406"){
-            window.location.href="https://drive.google.com/uc?id=1WE6Cyn7FhexfmiuXF6uggkF7174EWcsw"
-       }
-                 lse if(test.value == "A00988943"){
-            window.location.href="https://drive.google.com/uc?id=1o5tET7-EjUgGqv_HCTOKI-jBk621UvGW"
-       }
-                     else if(test.value == "10703060"){
-            window.location.href="https://drive.google.com/uc?id=1p16eTnI5C2wFfsBZhfELN6LZMlWXO_Rh"
-       }
-                      else if(test.value == "107030603"){
-            window.location.href="jakir.html"
-       }
-                       else if(test.value == "779966"){
-            window.location.href="mostafiz.html"
-       }
-            else{
-                 alert("invalid Passport")
-            }
-   
-              }
