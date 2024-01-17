4df21633b075149b55a47c333a226d800e797ea2
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2024-01-17 22:56:20 +0600

    Update app.js

--- app.js
+++ app.js
@@ -228,7 +228,10 @@ if(test.value == "demo123"){
          else if(test.value == "EB0869866"){
      window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYT7AWBbkB0T6_kLw6dwdgjm1RfrntnIJXz5HQPG3LJN1lcLojCu-0j93-n7laaV9TgocrhsyrXxHZPXHZBSaJU4uHjxyA=s1600"
 }
-                          else if(test.value == "A00351397"){
+          else if(test.value == "EH0457358"){
+     window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYQDISQtxzV5qmm4QyD4CwwgfvSmLZPvDa2XSHwhdCRzOu02SfPhu1kP8WHdB2Fp3Plcr82ON7YtV-z_uykynaH3-Mrtmg=s1600"
+}             
+         else if(test.value == "A00351397"){
      window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYRVvVtOThuh13hRVIYmrcOUDVn-B5O6sTu9d_6wvWsBqTNsVWUbNiW3qgDGUZ3Tww5nOaoa_0NPQdHv6oSs2a8Vy4zC=s1600"
 }   
                           else if(test.value == "EK0371347"){
