eca53771b4ed476b455de74a8d91ab36c9b5795d
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2024-02-11 21:54:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -366,7 +366,10 @@ if(test.value == "demo123"){
         else if(test.value == "A05863966"){
      window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYS4TM7Hp-u7HKnkHPVS_9tAKHHompNF52DfM4C1-OrOsDLghv5JMMydVfahJ6HSTQcT9iaTcV5WfzSVhA_NHoAfz6gj=s1600"
 }
-     else if(test.value == "A07302589"){
+     else if(test.value == "A08317619"){
+     window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYRLm527TTtojht9SxG9pABr7ruXJLVHSVh4zbJxxQ67d62QcM_rb0YgtUe6C2LD9pmMYetNrZL7ZGfh2F8GgycpWX3T=s1600"
+}
+        else if(test.value == "A07302589"){
      window.location.href="https://lh3.googleusercontent.com/drive-viewer/AEYmBYTTb5VfOEIw75o0VqZRiaicPgrVnqf-dOqez3KXziVujeJfSi9jHZ1JEXOyyVAXmX5qpVGz_FB6g88qUW8IEPQPMOSHtw=s1600"
 }
         else if(test.value == "B00021399"){
