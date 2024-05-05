47d3fdc28a42a9c030c32a677cc7681af125042f
canadavisaofficial <154331400+canadavisaofficial@users.noreply.github.com>
2024-05-05 15:25:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ if(test.value == "demo123"){
       else if(test.value == "A04088118"){
       window.location.href="belalhossain123.html"
 }
+      else if(test.value == "EE0259764"){
+      window.location.href="uazzalmiah123.html"
+}
       else if(test.value == "EE0069663"){
       window.location.href="delowarhossain123.html"
 }
